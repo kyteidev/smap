efe63688409cdad93a03e3b86c73d516c0a7cd89
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-01-13 20:21:10 +0200

    add input and tokyo night colors

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      bg: "#1a1b26",
+      fg: "#a9b1d6",
+    },
+  },
+  plugins: [],
+};
