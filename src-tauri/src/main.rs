9b4db4eae242abe5a7256dbbdb760a13a589e636
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-01-13 20:40:44 +0200

    add basic cli structure

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_cli::CliExt;
+
 fn main() {
-    smap_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_cli::init())
+        .setup(|app| {
+            match app.cli().matches() {
+                // `matches` here is a Struct with { args, subcommand }.
+                // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
+                // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
+                Ok(matches) => {
+                    println!("{:?}", matches)
+                }
+                Err(_) => {}
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
