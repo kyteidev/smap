d631e375e492bb8766a5ca4cbde420b53014c85b
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-01-14 17:26:09 +0200

    add app finding and display them

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,74 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Serialize;
 use tauri_plugin_cli::CliExt;
+
+use std::process::Command;
+
+#[derive(Serialize, Debug)]
+struct AppInfo {
+    name: String,
+    path: String,
+}
+
+#[tauri::command]
+fn list_applications() -> Result<Vec<AppInfo>, String> {
+    println!("Finding applications...");
+    let output = Command::new("mdfind")
+        .arg("kMDItemContentType==\"com.apple.application-bundle\"")
+        .output()
+        .map_err(|e| e.to_string())?;
+
+    if output.status.success() {
+        println!("Found applications");
+        let apps = String::from_utf8(output.stdout).map_err(|e| e.to_string())?;
+        let app_paths: Vec<&str> = apps
+            .split('\n')
+            .filter(|app| {
+                !app.is_empty()
+                    && (
+                        // Filter for common application directories
+                        app.starts_with("/Applications/")
+                            || app.starts_with("/System/Applications/")
+                            || app.starts_with(&format!(
+                                "{}/Applications/",
+                                std::env::var("HOME").unwrap_or_default()
+                            ))
+                    )
+            })
+            .collect();
+        let mut app_infos = Vec::new();
+
+        println!("Obtaining app metadata...");
+        for app_path in app_paths {
+            let name_output = Command::new("mdls")
+                .args(&["-name", "kMDItemDisplayName", "-raw", app_path])
+                .output()
+                .map_err(|e| e.to_string())?;
+
+            let name = String::from_utf8(name_output.stdout)
+                .unwrap_or_else(|_| "Unknown".into())
+                .trim()
+                .to_string();
+
+            app_infos.push(AppInfo {
+                name,
+                path: app_path.into(),
+            });
+        }
+
+        Ok(app_infos)
+    } else {
+        Err("Failed to list applications".into())
+    }
+}
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_cli::init())
         .setup(|app| {
             match app.cli().matches() {
-                // `matches` here is a Struct with { args, subcommand }.
-                // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
-                // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
                 Ok(matches) => {
                     println!("{:?}", matches)
                 }
@@ -18,6 +76,7 @@ fn main() {
             }
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![list_applications])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { invoke } from "@tauri-apps/api/core";
 import "./global.css";
+import { createSignal, For, onMount } from "solid-js";
 
 const App = () => {
+  const [appNames, setAppNames] = createSignal<string[]>([]);
+  const [appPaths, setAppPaths] = createSignal<string[]>([]);
+
+  const fetchApplications = async () => {
+    try {
+      const apps = await invoke<string[]>("list_applications");
+      setAppNames(apps.map((app: any) => app.name));
+      setAppPaths(apps.map((app: any) => app.path));
+    } catch (e) {
+      console.error("Error fetching applications:", e);
+    }
+  };
+
+  onMount(() => {
+    fetchApplications();
+  });
+
   return (
-    <div class="search-container">
+    <div class="search-container overflow-hidden">
       <input class="w-screen text-fg bg-bg p-2 focus:outline-none focus:border-none h-12 text-3xl align-middle inline-block rounded-t-xl"></input>
+      <div class="results-container overflow-auto">
+        <For each={appNames()}>
+          {(appName: string) => (
+            <div class="flex flex-row justify-between items-center bg-bg w-full h-12">
+              <div class="ml-2 text-xl text-fg">{appName}</div>
+            </div>
+          )}
+        </For>
+      </div>
     </div>
   );
 };
