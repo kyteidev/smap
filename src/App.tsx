76b6c0e8bfa4369c8ba55933493919ba352f0ae6
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-01-14 19:18:00 +0200

    fix: entire page is scrolled, input shouldnt move

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,9 @@ const App = () => {
   });
 
   return (
-    <div class="search-container overflow-hidden">
-      <input class="w-screen text-fg bg-bg p-2 focus:outline-none focus:border-none h-12 text-3xl align-middle inline-block rounded-t-xl"></input>
-      <div class="results-container overflow-auto">
+    <div class="search-container overflow-hidden h-screen w-screen flex flex-col rounded-b-xl">
+      <input class="w-full text-fg bg-bg p-2 focus:outline-none focus:border-none h-12 text-3xl align-middle inline-block rounded-t-xl"></input>
+      <div class="results-container overflow-auto flex-grow">
         <For each={appNames()}>
           {(appName: string) => (
             <div class="flex flex-row justify-between items-center bg-bg w-full h-12">
